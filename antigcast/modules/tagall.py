659f79aa561d728ed39417dd07b27c4b77eacbe1
KAGE <110266102+Rohidkage@users.noreply.github.com>
2024-04-26 22:35:36 +0700

    Update tagall.py

--- antigcast/modules/tagall.py
+++ antigcast/modules/tagall.py
@@ -1,87 +1,183 @@
+import os, logging, asyncio
+
 import random
-from asyncio import sleep
+from telethon import Button
+from telethon import TelegramClient, events
+from telethon.tl.types import ChannelParticipantAdmin
+from telethon.tl.types import ChannelParticipantCreator
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.errors import UserNotParticipantError
+from config import *
 
-import asyncio
 
-from antigcast import Bot
-from pyrogram import filters
-from pyrogram.types import Message
-from pyrogram.errors import FloodWait, UserNotParticipant
+api_id = API_ID
+api_hash = API_HASH
+bot_token = TOKEN
+kntl = TelegramClient('kynan', api_id, api_hash).start(bot_token=bot_token)
 
-from antigcast.config import *
-from antigcast.helpers.tools import *
-from antigcast.helpers.admins import *
-from antigcast.helpers.message import *
-from antigcast.helpers.database import *
 
-STATUS = enums.ChatMemberStatus
 spam_chats = []
+
 emoji = "😀 😃 😄 😁 😆 😅 😂 🤣 😭 😗 😙 😚 😘 🥰 😍 🤩 🥳 🤗 🙃 🙂 ☺️ 😊 😏 😌 😉 🤭 😶 😐 😑 😔 😋 😛 😝 😜 🤪 🤔 🤨 🧐 🙄 😒 😤 😠 🤬 ☹️ 🙁 😕 😟 🥺 😳 😬 🤐 🤫 😰 😨 😧 😦 😮 😯 😲 😱 🤯 😢 😥 😓 😞 😖 😣 😩 😫 🤤 🥱 😴 😪 🌛 🌜 🌚 🌝 🎲 🧩 ♟ 🎯 🎳 🎭💕 💞 💓 💗 💖 ❤️‍🔥 💔 🤎 🤍 🖤 ❤️ 🧡 💛 💚 💙 💜 💘 💝 🐵 🦁 🐯 🐱 🐶 🐺 🐻 🐨 🐼 🐹 🐭 🐰 🦊 🦝 🐮 🐷 🐽 🐗 🦓 🦄 🐴 🐸 🐲 🦎 🐉 🦖 🦕 🐢 🐊 🐍 🐁 🐀 🐇 🐈 🐩 🐕 🦮 🐕‍🦺 🐅 🐆 🐎 🐖 🐄 🐂 🐃 🐏 🐑 🐐 🦌 🦙 🦥 🦘 🐘 🦏 🦛 🦒 🐒 🦍 🦧 🐪 🐫 🐿️ 🦨 🦡 🦔 🦦 🦇 🐓 🐔 🐣 🐤 🐥 🐦 🦉 🦅 🦜 🕊️ 🦢 🦩 🦚 🦃 🦆 🐧 🦈 🐬 🐋 🐳 🐟 🐠 🐡 🦐 🦞 🦀 🦑 🐙 🦪 🦂 🕷️ 🦋 🐞 🐝 🦟 🦗 🐜 🐌 🐚 🕸️ 🐛 🐾 🌞 🤢 🤮 🤧 🤒 🍓 🍒 🍎 🍉 🍑 🍊 🥭 🍍 🍌 🌶 🍇 🥝 🍐 🍏 🍈 🍋 🍄 🥕 🍠 🧅 🌽 🥦 🥒 🥬 🥑 🥯 🥖 🥐 🍞 🥜 🌰 🥔 🧄 🍆 🧇 🥞 🥚 🧀 🥓 🥩 🍗 🍖 🥙 🌯 🌮 🍕 🍟 🥨 🥪 🌭 🍔 🧆 🥘 🍝 🥫 🥣 🥗 🍲 🍛 🍜 🍢 🥟 🍱 🍚 🥡 🍤 🍣 🦞 🦪 🍘 🍡 🥠 🥮 🍧 🍨".split(
     " "
 )
 
-def get_arg(message: Message):
-    msg = message.text
-    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
-    split = msg[1:].replace("\n", " \n").split(" ")
-    if " ".join(split[1:]).strip() == "":
-        return ""
-    return " ".join(split[1:])
 
-async def isAdmin(filter, client, update):
+@kntl.on(events.NewMessage(pattern="^/start$"))
+async def help(event):
+  helptext = "**Ada 2 Mode Tag All Cok, Kalo /tagall emot sange + nama user. kalo /all itu random emote tanpa nama user.**"
+  await event.reply(
+    helptext,
+    link_preview=False,
+    buttons=(
+      [
+        Button.url('Owner', 't.me/kagebunshiiin'),
+      ],
+      [
+        Button.url('Support', 't.me/suportkage'),
+        Button.url('Channel', 't.me/kagestore69'),
+      ],
+    )
+  )
+  
+@kntl.on(events.NewMessage(pattern="^/tagall ?(.*)"))
+async def mentionall(event):
+  chat_id = event.chat_id
+  if event.is_private:
+    return await event.respond("**Jangan private bego**")
+  
+  is_admin = False
+  try:
+    partici_ = await kntl(GetParticipantRequest(
+      event.chat_id,
+      event.sender_id
+    ))
+  except UserNotParticipantError:
+    is_admin = False
+  else:
+    if (
+      isinstance(
+        partici_.participant,
+        (
+          ChannelParticipantAdmin,
+          ChannelParticipantCreator
+        )
+      )
+    ):
+      is_admin = True
+  if not is_admin:
+    return await event.respond("**Lu bukan admin anjeng**")
+  
+  if event.pattern_match.group(1) and event.is_reply:
+    return await event.respond("**Minimal kasih pesan anjeng!!**")
+  elif event.pattern_match.group(1):
+    mode = "teks"
+    msg = event.pattern_match.group(1)
+  elif event.is_reply:
+    mode = "balas"
+    msg = await event.get_reply_message()
+    if msg == None:
+        return await event.respond("**Si anjeng dibilang kasih pesan !!**")
+  else:
+    return await event.respond("**Si anjeng dibilang kasih pesan !!**")
+  
+  spam_chats.append(chat_id)
+  usrnum = 0
+  usrtxt = ''
+  async for usr in kntl.iter_participants(chat_id):
+    if not chat_id in spam_chats:
+      break
+    usrnum += 1
+    usrtxt += f"🥵 [{usr.first_name}](tg://user?id={usr.id})\n"
+    if usrnum == 5:
+      if mode == "teks":
+        txt = f"{usrtxt}\n\n{msg}"
+        await kntl.send_message(chat_id, txt)
+      elif mode == "balas":
+        await msg.reply(usrtxt)
+      await asyncio.sleep(2)
+      usrnum = 0
+      usrtxt = ''
+  try:
+    spam_chats.remove(chat_id)
+  except:
+    pass
+
+
+@kntl.on(events.NewMessage(pattern="^/cancel$"))
+async def cancel_spam(event):
+  if not event.chat_id in spam_chats:
+    return await event.respond('**Bego orang gak ada tag all**')
+  else:
     try:
-        member = await client.get_chat_member(chat_id=update.chat.id, user_id=update.from_user.id)
-    except FloodWait as wait_err:
-        await sleep(wait_err.value)
-    except UserNotParticipant:
-        return False
+      spam_chats.remove(event.chat_id)
     except:
-        return False
+      pass
+    return await event.respond('**Iya Anjeng Nih Gua Stop.**')
 
-    return member.status in [STATUS.OWNER, STATUS.ADMINISTRATOR]
 
-Admin = filters.create(isAdmin)
+@kntl.on(events.NewMessage(pattern="^/all ?(.*)"))
+async def mentionall(event):
+  chat_id = event.chat_id
+  if event.is_private:
+    return await event.respond("**Jangan private bego**")
+  
+  is_admin = False
+  try:
+    partici_ = await kntl(GetParticipantRequest(
+      event.chat_id,
+      event.sender_id
+    ))
+  except UserNotParticipantError:
+    is_admin = False
+  else:
+    if (
+      isinstance(
+        partici_.participant,
+        (
+          ChannelParticipantAdmin,
+          ChannelParticipantCreator
+        )
+      )
+    ):
+      is_admin = True
+  if not is_admin:
+    return await event.respond("**Lu bukan admin anjeng**")
+  
+  if event.pattern_match.group(1) and event.is_reply:
+    return await event.respond("**Minimal kasih pesan anjeng!!**")
+  elif event.pattern_match.group(1):
+    mode = "teks"
+    msg = event.pattern_match.group(1)
+  elif event.is_reply:
+    mode = "balas"
+    msg = await event.get_reply_message()
+    if msg == None:
+        return await event.respond("**Si anjeng dibilang kasih pesan !!**")
+  else:
+    return await event.respond("**Si anjeng dibilang kasih pesan !!**")
+  
+  spam_chats.append(chat_id)
+  usrnum = 0
+  usrtxt = ''
+  async for usr in kntl.iter_participants(chat_id):
+    if not chat_id in spam_chats:
+      break
+    usrnum += 1
+    usrtxt += f"[{random.choice(emoji)}](tg://user?id={usr.id})"
+    if usrnum == 5:
+      if mode == "teks":
+        txt = f"{usrtxt}\n\n{msg}"
+        await kntl.send_message(chat_id, txt)
+      elif mode == "balas":
+        await msg.reply(usrtxt)
+      await asyncio.sleep(2)
+      usrnum = 0
+      usrtxt = ''
+  try:
+    spam_chats.remove(chat_id)
+  except:
+    pass
 
-@Bot.on_message(filters.command("tagall") & filters.group & Admin)
-async def tagall(client, message: Message):
-    await message.delete()
-    chat_id = message.chat.id
-    args = get_arg(message)
-    if not args:
-        args = "Hi!"
-    spam_chats.append(chat_id)
-    usrnum = 0
-    usrtxt = ""
-    m = client.get_chat_members(chat_id)
-    async for usr in m:
-        if not chat_id in spam_chats:
-            break
-        usrnum += 1
-        usrtxt += f"[{random.choice(emoji)}](tg://user?id={usr.user.id}) "
-        if usrnum == 5:
-            txt = f"**{args}**\n\n{usrtxt}"
-            try:
-                await client.send_message(chat_id, txt)
-            except FloodWait as e:
-                await sleep(e.value)
-                await client.send_message(chat_id, txt)
 
-            await sleep(2)
-            usrnum = 0
-            usrtxt = ""
-    try:
-        await client.send_message(chat_id, "**Proses Tag All selesai.")
-        spam_chats.remove(chat_id)
-    except:
-        pass
-
-@Bot.on_message(filters.command("cancel") & filters.group & Admin)
-async def untag(client, message: Message):
-    if not message.chat.id in spam_chats:
-        return await message.reply("**Sepertinya tidak ada tagall disini.**")
-    else:
-        try:
-            spam_chats.remove(message.chat.id)
-        except:
-            pass
-        return await message.reply("**Proses Tag All berhenti..**")
+#kntl.run_until_disconnected()
