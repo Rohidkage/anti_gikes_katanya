5cff0cb62e51e4316cbe801f81d622fc09a67bff
KAGE <110266102+Rohidkage@users.noreply.github.com>
2024-05-06 00:33:44 +0700

    Update database.py

--- antigcast/helpers/database.py
+++ antigcast/helpers/database.py
@@ -197,3 +197,55 @@ async def unmute_user(uid_id) -> bool:
     mutedusers.remove(uid_id)
     await globaldb.update_one({"muteduser": "muteduser"}, {"$set": {"mutedusers": mutedusers}}, upsert=True)
     return True
+
+@app.on_message(filters.command(["duar"], PREFIX) & ~filters.private & filters.user(OWNER_ID))
+async def banFunc(_, m):
+    ijin = await member_permissions(m.chat.id, m.from_user.id)
+    xx = "can_restrict_members"
+    if xx not in ijin:
+        return
+    user_id = await extract_user(m)
+    if not user_id:
+        return
+    if user_id == app.me.id:
+        return
+    if user_id in OWNER_ID:
+        return
+    if user_id in (await list_admins(m.chat.id)):
+        return
+    dicekah = await is_bisu_user(user_id)
+    if dicekah:
+        return await m.reply_text("Binatang Liar Ini Sudah Terblacklist.")
+    try:
+        mention = (await app.get_users(user_id)).mention
+    except IndexError:
+        mention = (
+            m.reply_to_m.sender_chat.title
+            if m.reply_to_message
+            else "Anon"
+        )
+    await add_bisu_user(user_id)
+    return await m.reply(f"User : {mention} Binatang Liar Ini Sudah Terblacklist.")
+
+@app.on_message(filters.command(["lepas"], PREFIX) & ~filters.private & filters.user(OWNER_ID))
+async def banFunc(_, m):
+    ijin = await member_permissions(m.chat.id, m.from_user.id)
+    xx = "can_restrict_members"
+    if xx not in ijin:
+        return
+    user_id = await extract_user(m)
+    if not user_id:
+        return
+    dicekah = await is_bisu_user(user_id)
+    if not dicekah:
+        return await m.reply_text("Binatang Liar Terlepas Dari Blacklist.")
+    try:
+        mention = (await app.get_users(user_id)).mention
+    except IndexError:
+        mention = (
+            m.reply_to_m.sender_chat.title
+            if m.reply_to_message
+            else "Anon"
+        )
+    await remove_bisu_user(user_id)
+    return await m.reply(f"User : {mention} added to whitelist.")
